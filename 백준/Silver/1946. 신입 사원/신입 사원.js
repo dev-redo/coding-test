87e1bd500862ab184bea639bde74869168c980e8
hsy <69149030+dev-redo@users.noreply.github.com>
2022-03-30 12:57:34 +0900

    [Silver I] Title: 신입 사원, Time: 4160 ms, Memory: 417012 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,37 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').slice(0, -1);
+const parsed = input.map(str => str.split(' ').map(c => parseInt(c)));
+
+const t = parsed.shift();
+const tests = [];
+
+for (let i = 0; i < parsed.length; i++) {
+    const line = parsed[i];
+    
+    if (line.length === 1) {
+        const [n] = line;
+        tests.push(parsed.slice(i + 1, i + n + 1).sort(([a], [b]) => a - b));
+        i += n;
+    }
+}
+
+// console.log(tests);
+
+const ans = [];
+
+for (let test of tests) {
+    let cnt = 0;
+    let upper = Number.MAX_SAFE_INTEGER;
+    
+    for (let i = 0; i < test.length; i++) {
+        const [a, b] = test[i];
+        
+        if (b < upper) {
+            cnt += 1;
+            upper = b;
+        }
+    }
+    
+    ans.push(cnt);
+}
+
+console.log(ans.join('\n'));
