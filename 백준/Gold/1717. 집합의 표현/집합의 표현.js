029d2580c4a90caebcbeecf004ceb80a5a7d9e5c
hsy <69149030+dev-redo@users.noreply.github.com>
2022-04-11 21:32:01 +0900

    Update 집합의 표현.js

--- 백준/Gold/1717. 집합의 표현/집합의 표현.js
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.js
@@ -52,60 +52,58 @@ function findParent(arr, a, b) {
 
 
 // 2. 트리 풀이
-//정답 코드
+const readline = require('readline');
 const input = [];
-const readline = require("readline");
-readline
-  .createInterface({
+
+readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
-  })
-  .on("line", (line) => {
-    input.push(line.split(' ').map((n) => +n));
-  })
-  .on("close", () => {
-    solve();
+    output: process.stdout
+}).on('line', (line) => {
+    input.push(line.split(' ').map(elem => +elem));
+}).on('close', () => {
+    solution();
     process.exit();
-  });
+});
 
 class Tree {
-  constructor(n) {
-    this.parent = Array(n + 1)
-      .fill(0)
-      .map((_, i) => i);
-  }
-
-  union(n1, n2) {
-    const a = this.find(n1);
-    const b = this.find(n2);
-    this.parent[b] = a;
-  }
-
-  find(node) {
-    if (this.parent[node] === node) {
-      return node;
+    constructor(n) {
+        this.parent = new Array(n+1)
+            .fill(0)
+            .map((_, i) => i);
     }
-    this.parent[node] = this.find(this.parent[node]);
-    return this.parent[node];
-  }
+    union(n1, n2) {
+        const a = this.find(n1);
+        const b = this.find(n2);
+        if (a < b) this.parent[b] = a;
+        else this.parent[a] = b;
+    }
+
+    find(node) {
+        if (this.parent[node] === node) {
+            return node;
+        }
+        this.parent[node] = this.find(this.parent[node]);
+        return this.parent[node];
+    }
 }
 
-const solve =() =>{
-  const [n, m] = input[0];
-  input.shift();
-  const tree = new Tree(n);
-  let answer = "";
-  for (let i = 0; i < m; ++i) {
-    const [a, b, c] = input[i];
-    if (a === 0) {
-      tree.union(b, c);
-    } else {
-      if (tree.find(b) === tree.find(c)) {
-        answer += "YES\n";
-      } else {
-        answer += "NO\n";
-      }
+function solution() {
+    const [n,m] = input.shift();
+    const tree = new Tree(n);
+
+    const answer = [];
+    for (let el of input) {
+        const [a, b, c] = el;
+        if (a === 0) {
+            tree.union(b, c)
+        }
+        else {
+            if (tree.find(b) === tree.find(c)) {
+                answer.push("YES");
+            } else {
+                answer.push("NO")
+            }
+        }
     }
-  }
-  console.log(answer);
+    console.log(answer.join("\n"))
 }
