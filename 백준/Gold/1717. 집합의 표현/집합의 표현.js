269cd34a3a49fc19a13086c4e5d5da6b26d2e761
hsy <69149030+dev-redo@users.noreply.github.com>
2022-04-11 21:29:17 +0900

    트리 풀이 추가

--- 백준/Gold/1717. 집합의 표현/집합의 표현.js
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.js
@@ -49,3 +49,63 @@ function findParent(arr, a, b) {
   if (a === b) return "YES";
   else return "NO";
 }
+
+
+// 2. 트리 풀이
+//정답 코드
+const input = [];
+const readline = require("readline");
+readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", (line) => {
+    input.push(line.split(' ').map((n) => +n));
+  })
+  .on("close", () => {
+    solve();
+    process.exit();
+  });
+
+class Tree {
+  constructor(n) {
+    this.parent = Array(n + 1)
+      .fill(0)
+      .map((_, i) => i);
+  }
+
+  union(n1, n2) {
+    const a = this.find(n1);
+    const b = this.find(n2);
+    this.parent[b] = a;
+  }
+
+  find(node) {
+    if (this.parent[node] === node) {
+      return node;
+    }
+    this.parent[node] = this.find(this.parent[node]);
+    return this.parent[node];
+  }
+}
+
+const solve =() =>{
+  const [n, m] = input[0];
+  input.shift();
+  const tree = new Tree(n);
+  let answer = "";
+  for (let i = 0; i < m; ++i) {
+    const [a, b, c] = input[i];
+    if (a === 0) {
+      tree.union(b, c);
+    } else {
+      if (tree.find(b) === tree.find(c)) {
+        answer += "YES\n";
+      } else {
+        answer += "NO\n";
+      }
+    }
+  }
+  console.log(answer);
+}
