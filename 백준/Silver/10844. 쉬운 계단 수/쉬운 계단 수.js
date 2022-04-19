8b378a406c3d3d927887501344b01169160b6674
hsy <69149030+dev-redo@users.noreply.github.com>
2022-04-20 05:15:59 +0900

    Update 쉬운 계단 수.js

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.js
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.js
@@ -13,17 +13,7 @@ for (let i=1; i<=9; i++) {
 
 for (let i=1; i<N; i++) {
     for (let j=0; j<=9; j++) {
-        let num = 0;
-        if (j === 0) {
-            num = DP[i-1][1];
-        }
-        else if (j === 9) {
-            num = DP[i-1][8];
-        }
-        else {
-            num = (DP[i-1][j-1] || 0) + (DP[i-1][j+1] || 0)
-        }
-        DP[i][j] = num % 1000000000;
+        DP[i][j] = (DP[i-1][j-1] || 0) + (DP[i-1][j+1] || 0) % 1000000000;
     }
 }
 
