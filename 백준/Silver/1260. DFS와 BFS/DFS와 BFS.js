3b5493ad8ce64bb9d192b5d3fd7305e89e315882
hsy <69149030+dev-redo@users.noreply.github.com>
2022-04-22 23:43:58 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 15088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,80 +1,54 @@
-// 그래프 구현
-class Graph{
-    constructor(){
-        this.adjacencyList = {};
-    }
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    }
-    addEdge(v1,v2) {
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
-    depthFirstSearch(start){
-        const result = [];
-        const visited = {};
-        const adjacencyList = this.adjacencyList;
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').slice(0, -1);
 
-        (function dfs(vertex){
-            if(!vertex) return;
-            visited[vertex] = true;
-            result.push(vertex);
-            adjacencyList[vertex].forEach(neighbor => {
-                if(!visited[neighbor]){
-                    return dfs(neighbor)
-                }
-            });
-        })(start);
+let [N, M, V] = input[0].split(" ").map(Number);
+let graph =  [...Array(N+1)].map(()=>[]);
 
-        return result;
-    }
-    breadthFirstSearch(start) {
-        const queue = [start];
-        const result = [];
-        const visited = {};
-        visited[start] = true;
-        const adjacencyList = this.adjacencyList;
-        
-        while(queue.length !== 0) {
-            const currVertex = queue.shift();
-            result.push(currVertex);
-            adjacencyList[currVertex].forEach(neighbor => {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    this.adjacencyList[neighbor].sort((a,b) => a-b);
-                    queue.push(neighbor);
-                }
-            });
+for (let i = 1; i <= M; i++) {
+  let [from, to] = input[i].split(" ").map(Number);
+  graph[from].push(to);
+  graph[to].push(from);
+}
+
+graph.forEach((element) => {
+  element.sort((a, b) => a - b);
+});
+
+// DFS
+const visited_dfs = new Array(N + 1).fill(false);
+const answer_dfs = [];
+
+function dfs(V) {
+    if (visited_dfs[V]) return;
+    answer_dfs.push(V);
+    visited_dfs[V] = true;
+    graph[V].forEach((next) => {
+        if (visited_dfs[next] === false) {
+            dfs(next);
         }
-        return result;
+    })
+}
+
+dfs(V);
+console.log(answer_dfs.join(" "));
+
+// // BFS
+const visited_bfs = new Array(N + 1).fill(false);
+const answer_bfs = [];
+
+function bfs(V) {
+    const queue = [V];
+    while (queue.length) {
+        const curr = queue.shift();
+        if (visited_bfs[curr] === true) continue;
+        visited_bfs[curr] = true;
+        answer_bfs.push(curr);
+        graph[curr].forEach((next) => {
+            if (visited_bfs[next] === false) {
+                queue.push(next);
+            }
+        })
     }
 }
 
-
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').slice(0, -1)
-const parsed = input.map(el => el.split(" ").map(target => Number(target)));
-
-const [N, M, V] = parsed.shift();
-const edge = parsed;
-
-// 그래프 생성
-const graph = new Graph();
-// 그래프 정점 추가
-for (let i = 1; i <= N; i++) {
-    graph.addVertex(i);
-}
-// 그래프 간선 넣기
-for (let i = 0; i < M; i++) {
-    const [v1, v2] = edge[i];
-    graph.addEdge(v1,v2);
-}
-
-// 각 정점에 연결된 정점들 오름차순 정렬
-Object.values(graph.adjacencyList).map(el => el.sort((a,b) => a-b));
-
-// DFS, BFS 결과
-const answer = [];
-answer.push(graph.depthFirstSearch(V).join(" "));
-answer.push(graph.breadthFirstSearch(V).join(" "));
-
-console.log(answer.join("\n"));
+bfs(V);
+console.log(answer_bfs.join(" "));
