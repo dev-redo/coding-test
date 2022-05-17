a90a31fc3fd39b31fe6be50218b47080171211d7
hsy <69149030+dev-redo@users.noreply.github.com>
2022-05-17 17:21:29 +0900

    Update 8. 모든 아나그램 찾기.js

--- 자바스크립트 알고리즘 문제풀이(인강)/ch5. 효율성(투포인터, 슬라이딩윈도우, 해쉬)/8. 모든 아나그램 찾기/8. 모든 아나그램 찾기.js
+++ 자바스크립트 알고리즘 문제풀이(인강)/ch5. 효율성(투포인터, 슬라이딩윈도우, 해쉬)/8. 모든 아나그램 찾기/8. 모든 아나그램 찾기.js
@@ -1,3 +1,4 @@
+// Bad 풀이 - 정렬 / 투포인터 이용 O(NM)
 function solution(s,t) {
   let answer = 0;
   const s_sort = s.toLowerCase();
@@ -25,3 +26,65 @@ function solution(s,t) {
 const s="bacaAacba";
 const t="abc";
 console.log(solution(s, t));
+
+
+---
+// 강의 풀이: Hash & Sliding Window && Two Pointers Algorithm - O(N)
+const s = 'bacaAacba';
+const t = 'abc';
+console.log(solution(s,t));
+
+function solution(s, t) {
+    const sH = new Map();
+    const tH = new Map();
+    const t_len = t.length - 1;
+    
+    // tH hashmap 생성
+    for (let i=0; i<t.length; i++) {
+        const el = t[i];
+        if (tH.has(el)) {
+            tH.set(el, tH.get(el)+1);
+        } else {
+            tH.set(el, 1);
+        }
+    }
+    
+    // sH 초기 hashMap 생성
+    for (let i=0; i < t_len; i++) {
+        const el = s[i];
+        if (sH.has(el)) {
+            sH.set(el, sH.get(el)+1);
+        } else {
+            sH.set(el, 1);
+        }
+    }
+    
+    // 슬라이딩 윈도우
+    let answer = 0;
+    let lt = 0;
+    for (let rt = t_len; rt < s.length; rt++) {
+        const el = s[rt];
+        if (sH.has(el)) {
+            sH.set(el, sH.get(el)+1);
+        } else {
+            sH.set(el, 1)
+        }
+        if (compareMaps(sH, tH)) answer += 1;
+        const lk = s[lt];
+        sH.set(lk, sH.get(lk)-1);
+        if (sH.get(lk) === 0) {
+            sH.delete(lk);
+        }
+        lt += 1;
+    }
+    return answer;
+}
+
+// 두 Map이 일치하는지 판별
+function compareMaps(map1, map2) {
+    if(map1.size!==map2.size) return false;
+    for(let [key, val] of map1){
+        if(!map2.has(key) || map2.get(key)!==val) return false;
+    }
+    return true;
+}
