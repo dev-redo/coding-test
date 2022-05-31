ebacc85c6ae9e72ff80fb42e952938e78bfe6020
hsy <69149030+dev-redo@users.noreply.github.com>
2022-05-31 19:41:45 +0900

    Update 5. Least Recently Used(카카오 캐시 문제 변형).js

--- 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/5. Least Recently Used(카카오 캐시 문제 변형).js
+++ 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/5. Least Recently Used(카카오 캐시 문제 변형).js
@@ -1,3 +1,4 @@
+// 내 풀이
 const s = 5;
 const nums = [1, 2, 3, 2, 6, 2, 3, 5, 7];
 console.log(solution(s, nums));
@@ -18,4 +19,37 @@ function solution(s, nums) {
         }
     })
     return answer;
+}
+
+
+// 강사님 풀이: 삽입정렬
+const size = 5;
+const nums = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(size, nums));
+
+function solution(size, nums) {
+    const cache = [...Array(size)].fill(0);
+    nums.forEach(x => {
+        let pos = -1;
+        for (let i=0; i<size; i++) {
+            // 현재 작업이 cache에 존재 -> cache hit
+            if (x === answer[i]) {
+                pos = i;
+            }
+        }
+        // cache miss
+        if (pos === -1) {
+            for (let i=size-1; i>=1; i--) {
+                answer[i] = answer[i-1];
+            }
+        }
+        else {
+            for (let i=pos; i>=1; i--) {
+                answer[i] = answer[i-1];
+            }
+        }
+        answer[0] = x;
+    })
+    
+    return nums;
 }
