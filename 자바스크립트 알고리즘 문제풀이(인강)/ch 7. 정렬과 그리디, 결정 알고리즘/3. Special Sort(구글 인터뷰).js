5d2ecd2e313e1fbf6debad033b4a18408c9ee5ad
hsy <69149030+dev-redo@users.noreply.github.com>
2022-05-31 14:52:28 +0900

    Update 3. Special Sort(구글 인터뷰).js

--- 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/3. Special Sort(구글 인터뷰).js
+++ 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/3. Special Sort(구글 인터뷰).js
@@ -1,3 +1,4 @@
+// 내 풀이: flag 변수를 두고 변수 swipe
 const n = 8;
 const nums = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(n, nums));
@@ -12,4 +13,21 @@ function solution(n, nums) {
         }
     }
     return nums;
+}
+
+// 강사님 풀이: 버블솔트
+// arr[i]가 양수고 arr[i+1]이 음수면 -> 서로 위치 변경
+let arr=[1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr));
+
+function solution(arr){
+    let answer=arr;
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=0; j<arr.length-i-1; j++){
+            if(arr[j]>0 && arr[j+1]<0){
+                [arr[j], arr[j+1]]=[arr[j+1], arr[j]];
+            }
+        }   
+    } 
+    return answer;
 }
