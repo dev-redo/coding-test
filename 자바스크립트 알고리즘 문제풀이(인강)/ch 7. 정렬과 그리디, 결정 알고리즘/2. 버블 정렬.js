3935bf4d8e9c4aec26787669f0f5bea4b12a80dd
hsy <69149030+dev-redo@users.noreply.github.com>
2022-05-31 14:16:10 +0900

    Update 2. 버블 정렬.js

--- 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/2. 버블 정렬.js
+++ 자바스크립트 알고리즘 문제풀이(인강)/ch 7. 정렬과 그리디, 결정 알고리즘/2. 버블 정렬.js
@@ -6,11 +6,7 @@ function solution(n, nums) {
     for (let pass=0; pass < n; pass++) {
         for (let i=0; i < n-pass; i++) {
             if (nums[i] > nums[i+1]) {
-                let a = nums[i];
-                let b = nums[i+1];
-                
-                nums[i] = b;
-                nums[i+1] = a;
+                [nums[i], nums[i+1]] = [nums[i+1], nums[i]];
             }
         }
     }
