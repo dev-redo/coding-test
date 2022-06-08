5901131ac250227a29bd81c1987b06733b0a8913
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-09 03:17:05 +0900

    Update 미로 탐색.js

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,21 +1,19 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').slice(0,-1);
 const NM = input.shift().split(" ");
-const [n,m] = NM.map(el => Number(el));
+const [r,c] = NM.map(el => Number(el));
 const board = input.map(s => s.split("").map(el => Number(el)));
 
 const dx=[1, 0, -1, 0];
 const dy=[0, 1, 0, -1];
 
-function solution(n,m,board) {
-    const row = m;
-    const column = n;
+function solution(r,c,board) {
     const queue = [];
-    const dist = [...Array(column)].map(() => Array(row).fill(-1));
+    const dist = [...Array(r)].map(() => Array(c).fill(-1));
     dist[0][0] = 0;
     
     let cnt = 0;
-    for (let i=0; i<column; i++) {
-        for (let j=0; j<row; j++) {
+    for (let i=0; i<r; i++) {
+        for (let j=0; j<c; j++) {
             const cell = board[i][j];
             if (cell === 0 || dist[i][j] > 0) continue;
             queue.push([i,j,dist[i][j]]);
@@ -24,7 +22,7 @@ function solution(n,m,board) {
                 for (let k=0; k<4; k++) {
                     const nx = x + dx[k];
                     const ny = y + dy[k];
-                    if (nx < 0 || ny < 0 || nx >= column || ny >= row) continue;
+                    if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
                     if (dist[nx][ny] >= 0 || board[nx][ny] !== 1) continue;
                     dist[nx][ny] = cnt + 1;
                     queue.push([nx,ny,dist[nx][ny]]);
@@ -32,7 +30,7 @@ function solution(n,m,board) {
             }
         }
     }
-    return dist[column-1][row-1] + 1;
+    return dist[r-1][c-1] + 1;
 }
 
-console.log(solution(n,m,board));
+console.log(solution(r,c,board));
