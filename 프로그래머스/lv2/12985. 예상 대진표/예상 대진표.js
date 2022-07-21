ba4e4f43e58dac6f0911b42ca41a06a7d5e12d30
hsy <69149030+dev-redo@users.noreply.github.com>
2022-07-21 19:31:21 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b)
+{
+    var answer = 0;
+    
+    while (a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        
+        answer += 1;
+    }
+    
+    return answer;
+}
