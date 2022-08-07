a44dd94a9455dc486481b99ea628f784362653a2
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-07 20:52:52 +0900

    [level 2] Title: 위장, Time: 0.10 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -0,0 +1,14 @@
+function solution(clothes) {
+    let answer = 1;
+    
+    const clothObj = {};
+    for (let i=0; i<clothes.length; i++) {
+        clothObj[clothes[i][1]] = (clothObj[clothes[i][1]] || 1) + 1;
+    }
+    
+    for (let key in clothObj) {
+        answer *= clothObj[key];
+    }
+    
+    return answer - 1;
+}
