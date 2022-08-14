92fe5f6b5c1ef97d44cf0836e08eff1094f7dc77
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-14 21:43:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,7 +1,17 @@
+// ex. 1231234, k=3
+
 function solution(number, k) {
-    const answer = [];
+    const answer = []; // 숫자들을 넣고 뺄 stack (k만큼 뺄 수 있다)
     
+    // 1 -> answer = [1], k=3
+    // 2 -> answer = [2], k=2
+    // 3 -> answer = [3], k=1
+    // 1 -> answer = [3,1], k=1
+    // 2 -> answer = [3,2], k=0
+    // 3 -> answer = [3,2,3], k=0
+    // 4 -> answer = [3,2,3,4], k=0
     for (let num of number) {
+        // k 0보다 크고, 이전의 num이 현재의 num보다 작을 때 -> 
         while (k > 0 && answer[answer.length - 1] < num) {
             answer.pop();
             k -= 1;
@@ -9,7 +19,11 @@ function solution(number, k) {
         answer.push(num);
     }
     
+    // return answer.join(""); // -> 안 되는 히든케이스 o
+    
     // 히든케이스(테케 12): number = 777, k = 1
-    // return answer.join("");
+    // 이 경우 계속 push만 이루어지므로, 마지막에 자릿수 k만큼 slice해주기
+    
+    // 777 -> (0,2 ) => 77
     return answer.join("").slice(0, number.length - k);
 }
