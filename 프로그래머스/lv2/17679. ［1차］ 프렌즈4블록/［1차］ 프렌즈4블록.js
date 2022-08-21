5be7fcdc86fd5b804571125c6868bee45d9f824c
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-21 19:05:30 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.56 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,9 +1,11 @@
 function solution(m, n, board) {
     board = board.map((v) => v.split(""));
     
+    // 블록 터트리기
     while (true) {
         const find = [];
         
+        // 터트릴 블록 PUSH
         for (let y=0; y<m-1; y+=1) {
             for (let x=0; x<n-1; x+=1) {
                 const pick = board[y][x];
@@ -18,22 +20,31 @@ function solution(m, n, board) {
             }
         }
         
+        // 제거된 블록(= 0) 개수 출력
         if (!find.length) {
           return [].concat(...board).filter((v) => !v).length;
         }
-
+        
+        // 터트린 블록을 0(= 빈공간)으로 치환
         find.forEach(([y,x]) => {
             board[y][x] = 0;
             board[y][x+1] = 0;
             board[y+1][x] = 0;
             board[y+1][x+1] = 0;
         })
-
+        
+        // 블록을 지운 후 아래로 떨어트리기
         for (let i=m-1; i>0; i-=1) {
+            // 현재 row에 빈 공간이 있는지 확인
+            // 없으면 그 다음 row로 이동
             if (!board[i].some(v => !v)) continue;
-
+            
+            // 있으면 블록을 아래로 이동시키기
             for (let j=0; j<n; j++) {
-                for (let k=i-1; k>=0 && !board[i][j]; k-=1) {
+                for (let k=i-1; k>=0; k-=1) {
+                    if (board[i][j]) break; // 현재 값이 빈공간이 아니면 break
+                    
+                    // 위에 남은 블럭이 있을 시 -> 아래로 이동시키기
                     if (board[k][j]) {
                         board[i][j] = board[k][j];
                         board[k][j] = 0;
