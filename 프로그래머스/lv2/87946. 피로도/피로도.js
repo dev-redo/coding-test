6c0e41ec6faaa093129a71475376b6aa0818625b
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-21 21:11:14 +0900

    [level 2] Title: 피로도, Time: 0.24 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,23 +1,38 @@
 
 function solution(k, dungeons) {
     const len = dungeons.length;
+    
+    // 1. visited 배열 만들기
+    // -> 던전을 방문하는 순서가 없으므로 dfs를 이용한다
     const visited = new Array(len).fill(false);
     
-    let clearCnt = 0;
+    let clearCnt = 0; // 방문한 던전
+    
+    // 2. 모든 경우의 수(방문순서)를 확인하기 위해 dfs 이용
+    
+    // clearCnt = 1
+    
+    // dfs(30, 2)
     const dfs = (k, currCnt) => {
+        // 3. 가장 던전을 많이 돈 횟수 구하기
         clearCnt = Math.max(clearCnt, currCnt);
         
+        // 던전들 loop돌리기
+        // [[80,20],[50,40],[30,10]]
+        // [50, 40] -> 80, 0
+        // dfs(40, 1)
+        // dfs(30, 2)
         for (let i=0; i<len; i++) {
             const [minK, useK] = dungeons[i];
-            
+            // 최소 필요 피로도에 충족 & 방문 x => 방문할 던전
             if (k >= minK && !visited[i]) {
                 visited[i] = true;
-                dfs(k - useK, currCnt + 1);
+                dfs(k - useK, currCnt + 1); // 남은 던전에 대해 dfs 돌리기
                 visited[i] = false;
             }
         }
     }
-    dfs(k, 0);
+    dfs(k, 0); // k = 80. => 피로도로 80, 0
     
     return clearCnt;
 }
