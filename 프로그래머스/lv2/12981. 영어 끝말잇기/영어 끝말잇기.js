9568741d3efc4e7a183282f25e03304dd4c280e2
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-28 21:36:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 29.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,5 +1,7 @@
+// return: [ 번호, 차례 ]
 function solution(n, words) {
-    let turn = 1;
+    let turn = 1; // 차례
+    
     for (let i=1; i<words.length; i+=1) {
         // 통과 조건
         // 1. 앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말하기
@@ -7,10 +9,10 @@ function solution(n, words) {
         const pass = (words[i][0] === words[i-1][words[i-1].length-1]) && !(words.slice(0,i).includes(words[i]));
         if (i % n === 0) { turn += 1 } // turn이 바뀌었을 때 += 1
         
-        if (!pass) {
+        if (!pass) { // 탈락자 발생 O
             return ([i % n + 1, turn]);
         }
     }
     
-    return [0,0];
+    return [0,0]; // 탈락자 발생 X
 }
