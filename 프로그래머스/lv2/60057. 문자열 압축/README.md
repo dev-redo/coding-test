ceefaad63c94efedbd4080d1cf17ff2fd59af944
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-29 03:44:10 +0900

    [level 2] Title: 문자열 압축, Time: 0.27 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/README.md
+++ 프로그래머스/lv2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=javascript) 
 
 ### 성능 요약
 
-메모리: 30.1 MB, 시간: 3.69 ms
+메모리: 29.9 MB, 시간: 0.27 ms
 
 ### 구분
 
--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,11 +1,12 @@
 function solution(s) {
     let answer = s.length;
-    for(i = 1 ; i <= Math.floor(s.length / 2); i++) {
-        let sentence = "";
-        let last = 0;
-        while(last < s.length) {
-           let cnt = 1;
-           while(s.slice(last,last+i) === s.slice(last+i,last+i+i)) {
+    let result = [];
+    for(i = 1 ; i < s.length+1 ; i++) {
+        sentence = "";
+        last = 0;
+        while(last<s.length) {
+           cnt = 1;
+           while(s.slice(last,last+i) == s.slice(last+i,last+i+i)) {
                  cnt++;
                  last = last+i; 
            }
@@ -14,9 +15,12 @@ function solution(s) {
            }
             sentence = sentence + s.slice(last,last+i);
             last = last+i;
-        }
-        answer = Math.min(answer, sentence.length);
+        } 
+        temp = {};
+        temp.sentence = sentence;
+        temp.len = sentence.length
+        result.push(temp);
     }
-    console.log("hi")
-    return answer;
+    result.sort((a, b) => a.len - b.len);     
+    return result[0].len;
 }
