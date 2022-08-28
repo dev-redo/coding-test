7c2a9b31c7d74743c9a26b79838856e36122a1a7
hsy <69149030+dev-redo@users.noreply.github.com>
2022-08-28 21:30:22 +0900

    [level 2] Title: 삼각 달팽이, Time: 106.66 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,13 +1,14 @@
 function solution(n) {
-    const stack = [...Array(n)].fill().map((_, idx) => Array(idx + 1));
+    const stack = [...Array(n)].fill().map((_, idx) => Array(idx + 1)); // 달팽이
     
-    let num = 1;
+    let num = 1; // 각 요소에 들어갈 값
     let [row, col] = [-1, 0];
     
+    // 달팽이 채우기
     for (let i=n; i>0; i-=3) {
-        for (let j=0; j<i; j++) { stack[++row][col] = num++; }
-        for (let j=0; j<i-1; j++) { stack[row][++col] = num++; }
-        for (let j=0; j<i-2; j++) { stack[--row][--col] = num++; }
+        for (let j=0; j<i; j++) { stack[++row][col] = num++; } // 왼쪽 아래
+        for (let j=0; j<i-1; j++) { stack[row][++col] = num++; } // 오른쪽 가로
+        for (let j=0; j<i-2; j++) { stack[--row][--col] = num++; } // 왼쪽 위
     }
     
     return stack.flat();
