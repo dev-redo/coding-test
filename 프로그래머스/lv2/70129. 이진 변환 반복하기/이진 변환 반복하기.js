c40d2e14609c6d2b2d487de44a1d87affb15d164
hsy <69149030+dev-redo@users.noreply.github.com>
2022-09-18 18:41:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.45 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,4 +1,18 @@
 function solution(s) {
-    const removeZero =  s.replaceAll('0', '');
-    return removeZero.length.toString(2)
+    return convertToBinary(s, 0, 0);
+}
+
+// input: s
+// output: 이진변환 횟수, 
+
+// 이진변환: x의 0 제거 => x의 길이를 2진변환
+const convertToBinary = (s, turnCount, removedCount) => {
+    if (s === '1') return [turnCount, removedCount];
+    const {removed, count} = removeZero(s);
+    return convertToBinary(removed.length.toString(2), turnCount + 1, removedCount + count)
+}
+
+const removeZero = s => {
+    const removed = s.replaceAll('0', '');
+    return {removed, count: s.length - removed.length}
 }
