71a3f5694c329baa235a8adc790a49d69f242ab2
hsy <69149030+dev-redo@users.noreply.github.com>
2022-09-19 03:01:45 +0900

    [level 2] Title: 후보키, Time: 34.35 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.js
+++ 프로그래머스/lv2/42890. 후보키/후보키.js
@@ -1,9 +1,17 @@
+// 풀이
+// 1. 모든 부분집합을 구하기
+// 2. 부분집합 중에서 유일성과 최소성을 만족하는 부분집합을 남기기
+// 3. 남겨진 부분집합의 개수를 구하기
 function solution(relation) {
-    const keys = [];
+    const keys = []; // 유일성과 최소성을 만족하는 부분집합을 담는 배열
+    
+    // 컬럼을 숫자로 변환 -> [0,1,2,3]
     let idxArr = Array.from(Array(relation[0].length), (_, i) => i);
     
+    // 가능한 모든 조합을 검사
     for (let i = 0 ; i < idxArr.length ; i++) {
-        const combinations = getCombinations(idxArr, i + 1);
+        const combinations = getCombinations(idxArr, i + 1); // [ [ 0 ], [ 1 ], [ 2 ], [ 3 ] ]
+        // 부분집합이 유일성과 최소성 통과할 시 배열에 추가
         for (const attrComb of combinations) {
             if (isMinimal(keys, attrComb) && isUnique(relation, attrComb)) {
                 keys.push(attrComb);
@@ -13,7 +21,6 @@ function solution(relation) {
     return keys.length;
 }
 
-// 조합 구하기: 백트래킹
 function getCombinations(arr, k) {
     const answer = [];
     const comb = [...Array(k)].fill(0);
@@ -32,7 +39,8 @@ function backTracking(answer, comb, arr, k, idx, start) {
     }
 }
 
-// 최소성 구하기
+// 최소성 판별
+// 유일성과 최소성을 갖춘 keys의 부분집합을 포함하는지 판별
 function isMinimal(keys, attrComb) {
     for (const key of keys) {
         if (key.every(attr => attrComb.includes(attr))) {
@@ -42,10 +50,13 @@ function isMinimal(keys, attrComb) {
     return true;
 }
 
+// 유일성 판별
+// 컬럼에 대해 모든 튜플이 유일성을 갖추는지 판별
 function isUnique(relation, attrIndexComb) {
     let result = Array.from(Array(relation.length), x => '');
     for (const attrIndex of attrIndexComb) {
         relation.forEach((row, rowIndex) => result[rowIndex] += row[attrIndex]);
     }
+    console.log(result)
     return result.length === [...new Set(result)].length
 }
