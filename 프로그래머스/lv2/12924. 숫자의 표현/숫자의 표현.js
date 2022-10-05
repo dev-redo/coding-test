8c7ddd117af56ccc21e224404f700e91fc00649e
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-05 21:29:19 +0900

    [level 2] Title: 숫자의 표현, Time: 1.60 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,16 +1,18 @@
 function solution(n) {
-    if (n === 1) return 1;
+    // if (n === 1) return 1;
     
     let answer = 0;
+    
     for (let i = 1; i <= n; i++) {
-        let sum = 0
+        let sum = 0;
         for (let j = i; j <= n; j++) {
-            sum += j
+            sum += j;
             if (sum >= n) {
-                if (sum === n) answer++
-                break
+                if (sum === n) answer++;
+                break;
             }
         }
     }
+    
     return answer;
 }
