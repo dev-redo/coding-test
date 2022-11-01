798a9d11fc6b0bb9eb4ff8f59a4ce7c1dd280151
hsy <69149030+dev-redo@users.noreply.github.com>
2022-11-01 21:25:05 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.96 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,9 +1,11 @@
 function solution(n, left, right) {
     var answer = [];
-    while (left <= right) {
-        const row = Math.floor(left / n);
-        const right = left++ % n;
-        answer.push(Math.max(row, right) + 1);
+    while (left <= right) { // 1. left ~ right 배열 채우기
+        const row = Math.floor(left / n); // 행
+        const col = left % n; // 열
+        answer.push(Math.max(row, col) + 1); // 원소 값 = 둘 중 큰 것 + 1
+        
+        left += 1;
     }
     return answer;
 }
