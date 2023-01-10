9d02e1000545022a47a8345b98aab9886a5747ed
hsy <69149030+dev-redo@users.noreply.github.com>
2023-01-10 19:58:36 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,17 @@
+function solution(arrayA, arrayB) {
+    return Math.max(getA(arrayA, arrayB), getA(arrayB, arrayA));
+}
+
+function getA(arrayA, arrayB) {
+    let answer = 0;
+    
+    [...arrayA].sort().forEach(val => answer = gcd(answer, val));
+
+    if(arrayB.some(val => val % answer === 0)) return 0;
+    return answer;
+}
+
+const gcd = (num1, num2) => {
+  let r = num1 % num2;
+  return num2 === 0 ? num1 : gcd(num2, r);
+};
