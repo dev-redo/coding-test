7cfc63b64e6592c80ba4163c2fd7f37f0a1e0a7f
hsy <69149030+dev-redo@users.noreply.github.com>
2023-02-25 19:18:34 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,27 +1,10 @@
-const WORDS = ["aya", "ye", "woo", "ma"];
-
 function solution(babbling) {
-    let answer = 0;
-    for (let word of babbling) {        
-        for (let idx=0; idx<WORDS.length; idx++) {
-            word = word.replaceAll(WORDS[idx], "O");
-        }
-        
-        const wordBabble = [...word.split('')
-                .reduce((set, elem) => {
-                    set.add(elem);
-                    return set;
-                }, new Set())];
-        const isWordBabble = wordBabble.length === 1 && wordBabble[0] === 'O';
-        
-        if (isWordBabble) { answer += 1; }
-    }
-    
-    return answer;
+  var answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach(word => {
+    if (regex.test(word)) answer++;  
+  })
+
+  return answer;
 }
-
-// "aya", "ye", "woo", "ma" 만 발음 가능
-// return input으로 주어지는 babbling에서 발음 가능한 단어의 개수
-
-// Algorithm Flow
-// 
